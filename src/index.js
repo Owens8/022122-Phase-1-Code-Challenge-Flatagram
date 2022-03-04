728cda0d934c052ad02f13249aacaee412ea1f24
Daniel Owens <owensdaniel8@yahoo.com>
2022-03-04 10:56:17 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// write your code here
+const images = "http://localhost:3000/images";
+const comments = "http://localhost:3000/comments";
+const cardImage = document.getElementById('card-image')
+const likesBttn = document.getElementById('like-button')
+const commentForm = document.getElementById('comment-form')
+const commentsList = document.getElementById('comments-list')
+
+
+
+
+fetch(images)
+    .then((res) => res.json())
+    .then(displayImage)
+    
+
+fetch(comments)
+    .then((res) => res.json())
+    .then(displayComments)
+
+function displayComments(commentsArray){
+
+}
+
+function displayImage(imageArray){
+    const dogImage = document.createElement("img");
+    dogImage.src = imageArray.image;
+    cardImage.append(dogImage);
+}
+
